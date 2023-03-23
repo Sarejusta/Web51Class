ce927e4c9eeb70df1585b24c67ee4b40b15a68d5
Sarejusta <audreydegiorgio@gmail.com>
2023-03-23 14:29:38 +0100

    Validation errors coding

--- assets/scripts/validation.js
+++ assets/scripts/validation.js
@@ -1,5 +1,53 @@
 function validateForm(){
 
-    alert('Validate Function called successfully');
-    
+    // single line comment
+    /*
+
+    this is
+    a multi-line
+    comment
+
+    */
+
+    var errors = "";
+
+    // alert('Validate Function called successfully');
+
+    // Get value of element and storing it in variable
+    var fullName = document.getElementById('fullName').value;
+    // check if fullName variable is empty
+    if(fullName==""){
+        //alert('Please enter your full name.');
+        // Block action from getting executed
+        //return false;
+        errors += "<li>Please enter your full name.";
+    }
+
+    var username = document.getElementById('username').value;
+    if(username==""){
+        //alert('Please enter your username.');
+        //return false;
+        errors += "<li>Please enter your username.";
+    }
+
+    var password = document.getElementById('password').value;
+    if(password==""){
+        //alert('Please enter your password.');
+        //return false;
+        errors += "<li>Please enter your password.";
+    }
+
+    var nationality = document.getElementById('nationality').value;
+    if(nationality=="0"){
+        //alert('Please select your nationality.');
+        //return false;
+        errors += "<li>Please enter your nationality.";
+    }
+
+    if (errors!= ""){
+        document.getElementById('errorList').innerHTML = errors;
+        return false;
+    }
+
+
 }
