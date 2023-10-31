0d8908c1ba45ec4f4f99475c8c5265e0eb604c18
Sarejusta <audreydegiorgio@gmail.com>
2023-10-31 09:47:29 +0100

    remove plis

--- assets/scripts/validation.js
+++ assets/scripts/validation.js
@@ -21,6 +21,10 @@ function validateForm(){
         // Block action from getting executed
         //return false;
         errors += "<li>Please enter your full name.";
+        document.getElementById('fullName').classList.add('inputError');
+    }
+    else{
+        document.getElementById('fullName').classList.remove('inputError');
     }
 
     var username = document.getElementById('username').value;
@@ -28,6 +32,10 @@ function validateForm(){
         //alert('Please enter your username.');
         //return false;
         errors += "<li>Please enter your username.";
+        document.getElementById('username').classList.add('inputError');
+    }
+    else{
+        document.getElementById('username').classList.remove('inputError');
     }
 
     var password = document.getElementById('password').value;
@@ -35,6 +43,10 @@ function validateForm(){
         //alert('Please enter your password.');
         //return false;
         errors += "<li>Please enter your password.";
+        document.getElementById('password').classList.add('inputError');
+    }
+    else{
+        document.getElementById('password').classList.remove('inputError');
     }
 
     var nationality = document.getElementById('nationality').value;
@@ -42,6 +54,10 @@ function validateForm(){
         //alert('Please select your nationality.');
         //return false;
         errors += "<li>Please enter your nationality.";
+        document.getElementById('nationality').classList.add('inputError');
+    }
+    else{
+        document.getElementById('nationality').classList.remove('inputError');
     }
 
     if (errors!= ""){
@@ -50,4 +66,12 @@ function validateForm(){
     }
 
 
+}
+
+function resetForm(){
+    document.getElementById('errorList').innerHTML = "";
+    document.getElementById('fullName').classList.remove('inputError');
+    document.getElementById('username').classList.remove('inputError');
+    document.getElementById('password').classList.remove('inputError');
+    document.getElementById('nationality').classList.remove('inputError');
 }
